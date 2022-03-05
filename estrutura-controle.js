bf959207a85c7d04766bf6366a48a03424b6b550
ASCII000 <r4n.oficial@gmail.com>
2022-03-05 10:40:48 -0300

    no coments font

--- estrutura-controle.js
+++ estrutura-controle.js
@@ -1,26 +1,3 @@
-/*
-//INPUTS
-escreval("Digite o nome do aluno:")
-leia(nome)
-
-escreval("Digite a nota 1 do aluno:")
-leia(nota1)
-
-escreval("Digite a nota 2 do aluno:")
-leia(nota2)
-
-//Operator
-
-media <- (nota1 + nota2) / 2
-
-//COMMAND
-se media >= 5 então
-   escreval("Aprovado ", nome)
-senao
-     escreval("Reprovado ", nome)
-fimse
-*/
-
 var nome, nota1, nota2;
 nome = prompt("Qual o nome do aluno?")
 nota1 = prompt("Qual é a primeira nota do aluno?")
